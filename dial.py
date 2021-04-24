d64a958caa6130a6dab2bfa2898c0037253a03dc
ClayLiu <hdifhd@qq.com>
2021-04-24 21:21:52 +0800

    At line 12 增加 -n 1 的参数，防止运行时间太长

--- dial.py
+++ dial.py
@@ -9,10 +9,10 @@ with open('config.txt', 'r') as f:
 
 
 while True:
-    res = run('ping www.baidu.com', shell = True)
+    res = run('ping www.baidu.com -n 1', shell = True)  # 增加 -n 1 的参数，防止运行时间太长
 
     if res.returncode:
-        run('rasdial Dr.com /DISCONNECT', shell = True)
+        # run('rasdial Dr.com /DISCONNECT', shell = True)   # 注释该条，以观察现象 2021-4-15 13:23:41
         run('rasdial Dr.com {} {}'.format(username, password), shell = True)
     
     sleep(waittime)
